08527d459fa63f1a02f3898bf06c3698cf8922b3
tanmayagrwl <tanmayhp78@gmail.com>
2023-03-15 18:17:21 +0530

    aminate right column

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 gsap.from(".header",{duration: 1, y:"-100%", ease:'bounce'} );
 
 gsap.from(".link", {duration : 1, opacity: 0, delay:1, Stagger: .5, });
+
+gsap.from(".right",{duration:1, x:'-100vw', delay:1, ease:"power2.out"})

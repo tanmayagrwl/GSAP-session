6b413fda80bb8032f86040ace068bbd8aac4c9a5
tanmayagrwl <tanmayhp78@gmail.com>
2023-03-15 18:11:44 +0530

    style links in header

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-gsap.from(".header",{duration: 1, y:"-100%", ease:'bounce'} )
+gsap.from(".header",{duration: 1, y:"-100%", ease:'bounce'} );
+
+gsap.from(".link", {duration : 1, opacity: 0, delay:1, Stagger: .5, });

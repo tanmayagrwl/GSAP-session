decb60e595801c711de6a8b7fc296bc6dc761c6b
tanmayagrwl <tanmayhp78@gmail.com>
2023-03-15 18:21:30 +0530

    animating footer using gsap.to

--- script.js
+++ script.js
@@ -2,4 +2,7 @@ gsap.from(".header",{duration: 1, y:"-100%", ease:'bounce'} );
 
 gsap.from(".link", {duration : 1, opacity: 0, delay:1, Stagger: .5, });
 
-gsap.from(".right",{duration:1, x:'-100vw', delay:1, ease:"power2.out"})
+gsap.from(".right",{duration:1, x:'-100vw', delay:1, ease:"power2.out"});
+
+gsap.from(".left",{duration:1, x:'-100%', delay:1.5, ease:"power2.out"});
+gsap.to(".footer",{duration:1, y:0, ease:"elastic", delay:2.5});

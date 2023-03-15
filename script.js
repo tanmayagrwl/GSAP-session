6465fe36b7454d5cf5b8b68707ff4c27ac87f683
tanmayagrwl <tanmayhp78@gmail.com>
2023-03-15 18:28:28 +0530

    animate button using ".fromTo"

--- script.js
+++ script.js
@@ -6,3 +6,8 @@ gsap.from(".right",{duration:1, x:'-100vw', delay:1, ease:"power2.out"});
 
 gsap.from(".left",{duration:1, x:'-100%', delay:1.5, ease:"power2.out"});
 gsap.to(".footer",{duration:1, y:0, ease:"elastic", delay:2.5});
+
+gsap.fromTo(".button",
+{opacity: 0, scale: 0, rotation:0},
+{duration: 1, delay: 3.5, opacity: 1, scale: 1, rotation:720}
+ );
